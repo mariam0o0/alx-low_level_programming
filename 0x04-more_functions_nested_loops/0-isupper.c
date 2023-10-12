0532c50862ed61b4931dd8f2fc4115e3980f45f2
Mariam <mariam.yasser772000@gmail.com>
2023-10-12 12:05:58 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
+#include"main.h"
+
+/**
+ * _isupper - t checks for uppercase character
+ * @c: the character
+ * Return: 1 for upper or 0 for lower
+ */
+
 int _isupper(int c)
 {
-if ((c >= 65) && (c <= 90))
-{
-return (1);
-}
-
-if ((c >= 97) && (c <= 122))
-{
-return (0);
-}
+	if ((c >= 65) && (c <= 90))
+		return (1);
+	if ((c >= 97) && (c <= 122))
+		return (0);
 }
