405abc1ada789ad1a0a42ff8b9b7a6590a97aaf7
Mariam <mariam.yasser772000@gmail.com>
2023-10-12 15:13:19 -0700

    t

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,17 +7,19 @@
 int main(void)
 {
 int n;
-
 for (n = 1; n <= 100; n++)
 {
 if ((n % 3) == 0 && (n % 5) == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if ((n % 3) == 0)
-printf("Fizz ");
+printf("Fizz");
 else if ((n % 5) == 0)
-printf("Buzz ");
+printf("Buzz");
 else
-printf("%i ", n);
+printf("%d", n);
+if (n == 100)
+continue;
+printf(" ");
 }
 printf("\n");
 return (0);
