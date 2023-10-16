b764a1baf987356312211b2f5d9d7ef0a4aac4a7
Mariam <mariam.yasser772000@gmail.com>
2023-10-16 09:17:33 -0700

    t

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_array - prints half of a string
+ * @a: the array
+ * @n: the number of elements of the array to be printed
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i == 0)
+printf("%d", a[i]);
+else
+printf(", %d", a[i]);
+}
+printf("\n");
+}
