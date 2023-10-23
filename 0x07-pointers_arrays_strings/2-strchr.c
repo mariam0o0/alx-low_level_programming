2d43ba8ee0b429ef9253c0a889c925c64ca72e5b
Mariam <mariam.yasser772000@gmail.com>
2023-10-23 08:40:22 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,20 +6,17 @@
  * @c: the character
  * Return: a pointer to the first occurrence of the character or NULL
  */
-
 char *_strchr(char *s, char c)
 {
-int i;
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
 
-i = 0;
-
-while (s[i] != '\0')
-{
-if (s[i] == c)
-return (s + i);
-
-i++;
-}
-
-return (NULL);
+	return (NULL);
 }
