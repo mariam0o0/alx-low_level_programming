51fe4adbda2257f4cf7edefe536b928c3594c860
Mariam <mariam.yasser772000@gmail.com>
2023-10-31 09:52:14 -0700

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * free_grid - frees a 2 dimensional grid previously created
  * @grid: input pointer to the array
@@ -13,8 +12,6 @@ void free_grid(int **grid, int height)
 	int i;
 
 	for (i = 0; i < height; i++)
-	{
 		free(grid[i]);
-	{
 	free(grid);
 }
