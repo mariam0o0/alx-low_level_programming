71deb664c2411849906a92cf0a717c5bb06238c3
Mariam <mariam.yasser772000@gmail.com>
2023-11-06 11:27:10 -0800

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,73 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
-  * new_dog - creates a new dog
-  * @name: the parameter name of the dog
-  * @age: the parameter age of the dog
-  * @owner: the parameter owner of the dog
-  * Return: returns pointer to dog
-  */
+ * _strlen - gives the lenght of string
+ * @s: pointer to the given string
+ * Return: the lenght
+ */
+int _strlen(char *s)
+{
+	int lenght;
+
+	lenght = 0;
+	while (s[lenght] != '\0')
+		lenght++;
+	return (lenght);
+}
+
+/**
+ * _strcpy - copies the string
+ * @dest: pointer to dest
+ * @src: pointer to source
+ * Return: the pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: the parameter name
+ * @age: the parameter age
+ * @owner: the parameter owner
+ * Return: pointer to new struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
-	dog_t *doggy;
+	dog_t *dog;
+	int len1, len2;
 
-	nlen = olen = 0;
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 		return (NULL);
 
-	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
-	for (i = 0; i < nlen; i++)
-		doggy->name[i] = name[i];
-
-	doggy->age = age;
-
-	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
 		return (NULL);
-	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];
-	return (doggy);
-}
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
+	}
+
