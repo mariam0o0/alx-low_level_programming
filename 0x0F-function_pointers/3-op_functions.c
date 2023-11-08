488db865c9ff239443b0f35191a6b3979ae68931
Mariam <mariam.yasser772000@gmail.com>
2023-11-08 11:21:45 -0800

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - addition
+ * @a: fist integer
+ * @b: second integer
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * op_sub - substraction
+ * @a: first integer
+ * @b: second integer
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_div - division
+ * @a: first integer
+ * @b: second integer
+ * Return: the result of division of a and b
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - dreminder of division
+ * @a: first integer
+ * @b: second integer
+ * Return: the reminder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
