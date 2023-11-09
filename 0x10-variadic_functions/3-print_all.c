0b800d613f2e862b081e48cd7505c0dc9b2676bc
Mariam <mariam.yasser772000@gmail.com>
2023-11-09 14:57:54 -0800

    task3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -29,7 +29,8 @@ void print_all(const char * const format, ...)
 			s = va_arg(args, char *);
 			if (!s)
 				printf("(nil)");
-			printf("%s", s);
+			else
+				printf("%s", s);
 			break;
 		default:
 			break;
