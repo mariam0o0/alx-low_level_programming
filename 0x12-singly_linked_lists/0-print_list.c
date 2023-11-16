b855c9d2c5205e74c58bcfa8bc361b650a296439
Mariam <mariam.yasser772000@gmail.com>
2023-11-16 08:31:54 -0800

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,9 @@ size_t elements = 0;
 while (h != NULL)
 {
 	if (h->str == NULL)
-		printf("[%d] %s\n", 0, "(nil)");
+		printf("[0] (nil)\n");
 	else
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 
 	h = h->next;
 	elements++;
