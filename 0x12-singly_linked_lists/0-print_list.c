df28c0ce608a34b1f1dd9bd6d2a9ad0abd4d23c1
Mariam <mariam.yasser772000@gmail.com>
2023-11-16 08:46:20 -0800

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,16 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t elements = 0;
+	size_t elements = 0;
 
-while (h != NULL)
-{
-	if (h->str == NULL)
-		printf("[0] (nil) \n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
-
-	h = h->next;
-	elements++;
-}
-
-return (elements);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
 }
