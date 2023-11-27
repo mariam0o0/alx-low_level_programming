0ce08dcd4d2808db95714a91c6504856ef741a90
Mariam <mariam.yasser772000@gmail.com>
2023-11-27 11:13:50 -0800

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,27 +14,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t bRead, bWritten;
 
-	if (!filename)
+	if ((!filename && letters))
+		return (0);
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
 		return (0);
 
-	fp = fopen(filename, "r");
-	if (!fp)
-		return (0);
-
-	buffer = malloc(letters + 1);
+	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
 	{
 		fclose(fp);
 		return (0);
 	}
-	bRead = fread(buffer, 1, letters, fp);
+	bRead = fread(buffer, letters, fp);
 	if (bRead == 0)
 	{
 		free(buffer);
 		fclose(fp);
 		return (0);
 	}
-	buffer[bRead] = '\0';
+	if (!b_read)
+		b_read = letters;
+
 	bWritten = write(fileno(stdout), buffer, bRead);
 	if (bWritten == -1 || bWritten != bRead)
 	{
