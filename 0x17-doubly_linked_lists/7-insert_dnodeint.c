8e5babda204b323e3ab10126d483386e273bce5a
Mariam <mariam.yasser772000@gmail.com>
2024-01-11 13:04:46 -0800

    0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int pos;
 	dlistint_t *new_node, *temp;
 
+	if (h == NULL)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	temp = *h;
