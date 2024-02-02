852c0e315385b7c2c9d79ff5cf1775ac2fb6ffd5
Mariam <mariam.yasser772000@gmail.com>
2024-02-02 10:51:59 -0800

    0x1A

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,15 +9,20 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 
 	unsigned long int index;
+	hash_node_t *item;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[index] != NULL)
-	{
-		if (strcmp(ht->array[index]->key, key) == 0)
-			return (ht->array[index]->value);
-	}
-	return (NULL);
+	item = ht->array[index];
+
+	while (item != NULL && strcmp(item->key, key) != 0)
+		item = item->next;
+
+	if (item == NULL)
+		return (NULL);
+	else
+		return (item->value);
+
 }
