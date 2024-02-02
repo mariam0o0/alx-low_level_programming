6b3629a2265ab558b8515bc6419bbb350f3ac646
Mariam <mariam.yasser772000@gmail.com>
2024-02-02 13:18:44 -0800

    0x1A

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,6 +9,9 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *temp;
 	unsigned long int i;
 
+	if (ht == NULL)
+		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
 		while (ht->array[i])
@@ -19,7 +22,7 @@ void hash_table_delete(hash_table_t *ht)
 			free(ht->array[i]);
 			ht->array[i] = temp;
 		}
-		free(ht->array);
-		free(ht);
 	}
+	free(ht->array);
+	free(ht);
 }
