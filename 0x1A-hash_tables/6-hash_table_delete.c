b38f9f1cc8a8924b410fd12834c303356126c5bc
Mariam <mariam.yasser772000@gmail.com>
2024-02-02 13:12:09 -0800

    0x1A

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			temp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = temp;
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
