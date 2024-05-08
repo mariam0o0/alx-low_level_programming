0d936c6a50e8709539d8c0250fc62287217aad70
Mariam <mariam.yasser772000@gmail.com>
2024-05-08 04:20:19 -0700

    0x1E

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (left = 0, right = size - 1; left <= right)
+	for (left = 0, right = size - 1; left <= right;)
 	{
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
